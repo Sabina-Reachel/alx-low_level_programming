9dd2d5e60ef228e9e8c6c3e83914bbff0c259352
Sabina-Reachel <sabinagu29@gmail.com>
2022-09-13 11:38:33 -0700

    0-putchar.c

---
+++ 0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include "_putchar.h"
+
+/**
+ * main - prints _putchar
+ *
+ * description: prints _putchar
+ *
+ * Return: 0 (Success);
+ */
+int main(void)
+{
+	char *sh = "_putchar";
+	
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
+
