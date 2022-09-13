06562a48639a4863a7b94c752c348eaa793b8783
Sabina-Reachel <sabinagu29@gmail.com>
2022-09-13 15:56:43 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,15 +3,11 @@
 /**
  * _isalpha - a function that checks alphabet
  * @c: single letter input
- * Return: 1 if c is alphabet, 0 if otherwise
+ * Return: 1 if c is a letter (lower or uppercase),0 if otherwise
  */
 int _isalpha(int c)
 {
-	if (c <= 'z' && c <= 'Z')
-		return (1);
-	else
-		return (0);
-	if (c >= 'A' && c <= 'Z')
+	if (((c <= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
 		return (1);
 	else
 		return (0);
