6565bb5aae09c8d4331ccb055dc2551685d7e54d
Sabina-Reachel <sabinagu29@gmail.com>
2022-09-13 15:47:13 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,7 +7,7 @@
  */
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if (c <= 'z' && c <= 'Z')
 		return (1);
 	else
 		return (0);
@@ -16,5 +16,4 @@ int _isalpha(int c)
 	else
 		return (0);
 
-}
-	
+}	
