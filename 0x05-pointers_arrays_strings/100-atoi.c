9a0a2ef06c46bc5aa06eca36b6fc67c433050c4c
Sabina-Reachel <sabinagu29@gmail.com>
2022-09-19 18:20:15 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,11 +2,29 @@
 
 /**
  * _atoi - Entry point
- * @*s: input
+ * @s: input
  * Return: Always 0 (Success)
  */
 int _atoi(char *s)
 {
+	int i, sign;
+	unsigned int number;
 
-	return (0);
+	sign = 1;
+	i = 0;
+	number = 0;
+
+
+	while ((s[i] < '0' || s[i] > '9') && (s[i] != '\0'))
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while ((s[i] >= '0') && (s[i] <= '9'))
+	{
+		number = number * 10 + (s[i] - '0');
+		i++;
+	}
+	return (number * sign);
 }
