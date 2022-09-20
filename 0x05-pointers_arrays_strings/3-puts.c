ef2c5d2047c2d47a1058f1e0544711c2b537c952
Sabina-Reachel <sabinagu29@gmail.com>
2022-09-19 17:25:32 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string, followed by a new line
+ * @str: string imput
+ * Return: string
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0, char[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
