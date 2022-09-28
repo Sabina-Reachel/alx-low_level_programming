3ba95d4222fe7e7d6499d16ddacbd00255fbdbd6
Sabina-Reachel <sabinagu29@gmail.com>
2022-09-28 08:44:54 -0700

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,7 @@
 #define MAIN_H
+#ifndef MAIN_H
 
+int _putchar(char.c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -11,4 +13,4 @@ void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 int _putchar(char.c);
 
-#endif
+#endif /*MAIN_H*/
