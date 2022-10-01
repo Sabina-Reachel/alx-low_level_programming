cf7993ff0b6ab0fafc915fad75cd74dedc27ca4f
Sabina-Reachel <sabinagu29@gmail.com>
2022-10-01 16:22:42 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -12,13 +12,17 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	if (argc == 1)
-		printf(" % d\n", argc - 1);
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			printf("argv[%d] = %s\n", i, argv[i]);
+		}
+	}
 	else
 	{
-		for (i = 0; *argv; i++, argv++);
 
-		printf(" % d\n", i - 1);
+		printf(" the command has no other arguments.\n");
 	}
 
 	return (0);
